05bdbdb69ae8c0b92294c4aac2b793ccec95d55a
boblovespi <boblovespi3.14@gmail.com>
2025-01-27 09:21:29 +0800

    brick crucible model

--- src/main/java/boblovespi/factoryautomation/data/FABlockStateProvider.java
+++ src/main/java/boblovespi/factoryautomation/data/FABlockStateProvider.java
@@ -72,7 +72,7 @@ public class FABlockStateProvider extends BlockStateProvider
 		castingVessel(FABlocks.STONE_CASTING_VESSEL);
 		existingBlockWithItem(FABlocks.STONE_WORKBENCH);
 		existingBlockModel(FABlocks.BRICK_MAKER_FRAME);
-		horizontalBlock(FABlocks.BRICK_CRUCIBLE.get(), multiblockComplete("brick_crucible", "brick_foundry_multiblock"), 270);
+		horizontalBlock(FABlocks.BRICK_CRUCIBLE.get(), multiblockComplete("brick_crucible", "brick_foundry_multiblock"));
 		horizontalBlock(FABlocks.BRICK_FIREBOX.get(), modLoc("block/brick_firebox_side"), modLoc("block/brick_firebox_front"), modLoc("block/brick_firebox_top"));
 		blockWithItem(FABlocks.CREATIVE_MECHANICAL_SOURCE);
 		axisOnlyBlock(FABlocks.WOOD_POWER_SHAFT, modLoc("block/power_shaft"), mcLoc("block/oak_planks"), mcLoc("block/oak_planks"));
