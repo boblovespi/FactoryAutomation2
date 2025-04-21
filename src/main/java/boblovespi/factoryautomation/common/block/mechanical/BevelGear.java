9f1d8b225168a2af8cf678437c5f6042a7c83aa3
boblovespi <boblovespi3.14@gmail.com>
2025-04-21 09:45:51 +0800

    bevel gear hitbox

--- src/main/java/boblovespi/factoryautomation/common/block/mechanical/BevelGear.java
+++ src/main/java/boblovespi/factoryautomation/common/block/mechanical/BevelGear.java
@@ -8,6 +8,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.FrontAndTop;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
@@ -18,6 +19,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 public class BevelGear extends Block implements EntityBlock
@@ -25,6 +29,26 @@ public class BevelGear extends Block implements EntityBlock
 	public static final EnumProperty<FrontAndTop> ORIENTATION = BlockStateProperties.ORIENTATION;
 	public final float maxSpeed;
 	public final float maxTorque;
+	private static final VoxelShape NORTH = Block.box(4, 4, 0, 12, 12, 12);
+	private static final VoxelShape SOUTH = Block.box(4, 4, 4, 12, 12, 16);
+	private static final VoxelShape EAST = Block.box(4, 4, 4, 16, 12, 12);
+	private static final VoxelShape WEST = Block.box(0, 4, 4, 12, 12, 12);
+	private static final VoxelShape UP = Block.box(4, 4, 4, 12, 16, 12);
+	private static final VoxelShape DOWN = Block.box(4, 0, 4, 12, 12, 12);
+	private static final VoxelShape[] COLLISON_BOXES = new VoxelShape[] {
+			Shapes.or(DOWN, EAST),
+			Shapes.or(DOWN, NORTH),
+			Shapes.or(DOWN, SOUTH),
+			Shapes.or(DOWN, WEST),
+			Shapes.or(UP, EAST),
+			Shapes.or(UP, NORTH),
+			Shapes.or(UP, SOUTH),
+			Shapes.or(UP, WEST),
+			Shapes.or(WEST, SOUTH),
+			Shapes.or(EAST, NORTH),
+			Shapes.or(NORTH, WEST),
+			Shapes.or(SOUTH, EAST)
+	};
 
 	public BevelGear(Properties properties, float maxSpeed, float maxTorque)
 	{
@@ -57,6 +81,12 @@ public class BevelGear extends Block implements EntityBlock
 		}
 		else
 			return defaultBlockState().setValue(ORIENTATION, FrontAndTop.fromFrontAndTop(dir, Direction.UP));
+	}
+
+	@Override
+	protected VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext)
+	{
+		return COLLISON_BOXES[pState.getValue(ORIENTATION).ordinal()];
 	}
 
 	@Nullable
