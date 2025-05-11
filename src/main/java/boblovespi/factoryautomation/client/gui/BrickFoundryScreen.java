cd287f133f1c52c04b384e04a2cf2813509706ec
Laz the Artist <33374497+Laz-The-Artist@users.noreply.github.com>
2025-05-11 10:50:56 +0200

    brick foundry gui molten metal texture

--- src/main/java/boblovespi/factoryautomation/client/gui/BrickFoundryScreen.java
+++ src/main/java/boblovespi/factoryautomation/client/gui/BrickFoundryScreen.java
@@ -4,12 +4,14 @@ import boblovespi.factoryautomation.FactoryAutomation;
 import boblovespi.factoryautomation.common.menu.BrickFoundryMenu;
 import boblovespi.factoryautomation.common.util.Form;
 import boblovespi.factoryautomation.common.util.Metal;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.InventoryMenu;
 
 public class BrickFoundryScreen extends AbstractContainerScreen<BrickFoundryMenu>
 {
@@ -18,6 +20,7 @@ public class BrickFoundryScreen extends AbstractContainerScreen<BrickFoundryMenu
 	private final GuiBar temperatureBar;
 	private final GuiBar progressBar;
 	private final GuiBar bellowsBar;
+	private final GuiTexturedBar metalBar;
 
 	public BrickFoundryScreen(BrickFoundryMenu pMenu, Inventory pPlayerInventory, Component pTitle)
 	{
@@ -28,6 +31,8 @@ public class BrickFoundryScreen extends AbstractContainerScreen<BrickFoundryMenu
 		temperatureBar = new GuiBar(BACKGROUND_TEXTURE, 53, 16, 176, 17, 6, 61, GuiBar.ProgressDirection.UP);
 		progressBar = new GuiBar(BACKGROUND_TEXTURE, 84, 21, 194, 2, 22, 10, GuiBar.ProgressDirection.RIGHT);
 		bellowsBar = new GuiBar(BACKGROUND_TEXTURE, 86, 61, 190, 14, 16, 14, GuiBar.ProgressDirection.LEFT);
+		var sprite = Minecraft.getInstance().getModelManager().getAtlas(InventoryMenu.BLOCK_ATLAS).getSprite(FactoryAutomation.name("block/molten_metal"));
+		metalBar = new GuiTexturedBar(107, 17, 16, 59, sprite, 16, 16);
 	}
 
 	@Override
@@ -47,7 +52,7 @@ public class BrickFoundryScreen extends AbstractContainerScreen<BrickFoundryMenu
 		progressBar.draw(this, graphics, Float.intBitsToFloat(menu.getData(2)));
 		bellowsBar.draw(this, graphics, Float.intBitsToFloat(menu.getData(6)));
 		var metal = Metal.fromId(menu.getData(4));
-		graphics.fill(leftPos + 107, topPos + (int) (76 - 59 * menu.getData(3) / (Form.INGOT.amount() * 9 * 3f)), leftPos + 123, topPos + 76, metal.color());
+		metalBar.draw(this, graphics, menu.getData(3) / (Form.INGOT.amount() * 9 * 3f), metal.color());
 	}
 
 	@Override
