1c6578fff72fb429737b807067112465745755c7
boblovespi <boblovespi3.14@gmail.com>
2025-06-03 14:37:07 +0800

    bevel gear render

--- src/main/java/boblovespi/factoryautomation/client/ClientHandler.java
+++ src/main/java/boblovespi/factoryautomation/client/ClientHandler.java
@@ -58,6 +58,7 @@ public class ClientHandler
 		// event.registerBlockEntityRenderer(FABETypes.BRICK_MAKER_FRAME_TYPE.get(), BrickMakerFrameBER::new);
 		event.registerBlockEntityRenderer(FABETypes.POWER_SHAFT_TYPE.get(), PowerShaftBER::new);
 		event.registerBlockEntityRenderer(FABETypes.GEARBOX_TYPE.get(), GearboxBER::new);
+		event.registerBlockEntityRenderer(FABETypes.BEVEL_GEAR_TYPE.get(), BevelGearBER::new);
 		event.registerBlockEntityRenderer(FABETypes.MILLSTONE_TYPE.get(), MillstoneBER::new);
 		event.registerBlockEntityRenderer(FABETypes.HANDCRANK_TYPE.get(), HandCrankBER::new);
 		event.registerBlockEntityRenderer(FABETypes.PAPER_BELLOWS_TYPE.get(), PaperBellowsBER::new);
---
+++ src/main/java/boblovespi/factoryautomation/client/ber/BevelGearBER.java
@@ -0,0 +1,100 @@
+package boblovespi.factoryautomation.client.ber;
+
+import boblovespi.factoryautomation.common.block.FABlocks;
+import boblovespi.factoryautomation.common.block.mechanical.BevelGear;
+import boblovespi.factoryautomation.common.block.mechanical.PowerShaft;
+import boblovespi.factoryautomation.common.blockentity.mechanical.BevelGearBE;
+import boblovespi.factoryautomation.common.item.FAItems;
+import boblovespi.factoryautomation.common.util.GearMaterial;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class BevelGearBER implements BlockEntityRenderer<BevelGearBE>
+{
+	private final BlockRenderDispatcher blockRenderer;
+	private final ItemRenderer itemRenderer;
+	private final BlockState[] shaftStates;
+	private final ItemStack[] renderStacks;
+
+	public BevelGearBER(BlockEntityRendererProvider.Context pContext)
+	{
+		blockRenderer = pContext.getBlockRenderDispatcher();
+		itemRenderer = pContext.getItemRenderer();
+		shaftStates = new BlockState[Direction.Axis.VALUES.length];
+		renderStacks = new ItemStack[] {FAItems.GEARS.get(GearMaterial.COPPER).toStack(), FAItems.GEARS.get(GearMaterial.IRON).toStack()};
+	}
+
+	@Override
+	public void render(BevelGearBE be, float delta, PoseStack stack, MultiBufferSource bufferSource, int light, int overlay)
+	{
+		var orientation = be.getBlockState().getValue(BevelGear.ORIENTATION);
+		var front = orientation.front();
+		var isHorizontal = front.getAxis() != Direction.Axis.Y;
+		var side = isHorizontal ? front.getCounterClockWise() : orientation.top();
+		var axis = front.getAxis();
+		if (shaftStates[axis.ordinal()] == null)
+			shaftStates[axis.ordinal()] = FABlocks.WOOD_POWER_SHAFT.get().defaultBlockState().setValue(PowerShaft.AXIS, axis);
+		var axis2 = side.getAxis();
+		if (shaftStates[axis2.ordinal()] == null)
+			shaftStates[axis2.ordinal()] = FABlocks.WOOD_POWER_SHAFT.get().defaultBlockState().setValue(PowerShaft.AXIS, axis);
+		var invIn = front == Direction.DOWN || front == Direction.EAST || front == Direction.NORTH ? 1 : -1;
+		var invOut = side == Direction.DOWN || side == Direction.EAST || front == Direction.NORTH ? 1 : -1;
+
+		stack.pushPose();
+		var scale = 0.28f;
+		{
+			stack.translate(0.5, 0.5, 0.5);
+			stack.mulPose(front.getRotation());
+			stack.translate(0, 2 / 16f * invIn * invOut, 0);
+			stack.mulPose(BERUtils.quatFromAngleAxis(90, 1, 0, 0));
+			stack.mulPose(BERUtils.quatFromAngleAxis(be.getRenderRot(delta), 0, 0, invIn));
+			stack.scale(scale, scale, scale);
+			itemRenderer.renderStatic(renderStacks[0], ItemDisplayContext.NONE, light, overlay, stack, bufferSource, be.getLevel(), 42);
+		}
+		stack.popPose();
+
+		stack.pushPose();
+		{
+			stack.translate(0.5, 0.5, 0.5);
+			stack.mulPose(side.getRotation());
+			stack.translate(0, 2 / 16f * invIn * invOut * -1, 0);
+			stack.mulPose(BERUtils.quatFromAngleAxis(90, 1, 0, 0));
+			stack.mulPose(BERUtils.quatFromAngleAxis(be.getRenderRot(delta) + 22.5f, 0, 0, invOut));
+			stack.scale(scale, scale, scale);
+			itemRenderer.renderStatic(renderStacks[1], ItemDisplayContext.NONE, light, overlay, stack, bufferSource, be.getLevel(), 42);
+		}
+		stack.popPose();
+
+		stack.pushPose();
+		{
+			stack.translate(0.5, 0.5, 0.5);
+			stack.mulPose(front.getRotation());
+			stack.mulPose(BERUtils.quatFromAngleAxis(-be.getRenderRot(delta), 0, invIn, 0));
+			stack.translate(0, 2/16f * invIn * invOut, 0);
+			stack.scale(0.5f, 6/16f - 2/16f * invIn * invOut, 0.5f);
+			blockRenderer.renderBatched(shaftStates[axis.ordinal()], be.getPos(), be.getLevel(), stack, bufferSource.getBuffer(RenderType.SOLID), false, RandomSource.create(42));
+		}
+		stack.popPose();
+
+		stack.pushPose();
+		{
+			stack.translate(0.5, 0.5, 0.5);
+			stack.mulPose(side.getRotation());
+			stack.mulPose(BERUtils.quatFromAngleAxis(-be.getRenderRot(delta), 0, invOut, 0));
+			stack.translate(0, -2/16f * invIn * invOut, 0);
+			stack.scale(0.5f, 6/16f + 2/16f * invIn * invOut, 0.5f);
+			blockRenderer.renderBatched(shaftStates[axis2.ordinal()], be.getPos(), be.getLevel(), stack, bufferSource.getBuffer(RenderType.SOLID), false, RandomSource.create(42));
+		}
+		stack.popPose();
+	}
+}
